353690f7f5d054013e6e5b5434b9dc8a42f7933d
Martin Fleming <martinflemingdev@gmail.com>
2026-04-13 17:02:21 -0400

    add google_bigquery_analytics_hub_listing_iam_member resource

--- config/externalname.go
+++ config/externalname.go
@@ -1065,6 +1065,8 @@ var terraformPluginSDKExternalNameConfigs = map[string]config.ExternalName{
 	"google_bigquery_analytics_hub_data_exchange_iam_member": config.IdentifierFromProvider,
 	// Imported by using the following projects/{{project}}/locations/{{location}}/subscriptions/{{subscription_id}}
 	"google_bigquery_analytics_hub_listing_subscription": config.IdentifierFromProvider,
+	// Imported by using the following projects/{{project}}/locations/{{location}}/dataExchanges/{{data_exchange_id}}/listings/{{listing_id}} roles/viewer user:jane@example.com
+	"google_bigquery_analytics_hub_listing_iam_member": config.IdentifierFromProvider,
 
 	// tpu
 	//
--- config/namespaced/bigquery/config.go
+++ config/namespaced/bigquery/config.go
@@ -103,4 +103,12 @@ func Configure(p *config.Provider) {
 			return diff, nil
 		}
 	})
+	p.AddResourceConfigurator("google_bigquery_analytics_hub_listing_iam_member", func(r *config.Resource) {
+		r.References["listing_id"] = config.Reference{
+			TerraformName: "google_bigquery_analytics_hub_listing",
+		}
+		r.References["data_exchange_id"] = config.Reference{
+			TerraformName: "google_bigquery_analytics_hub_data_exchange",
+		}
+	})
 }
